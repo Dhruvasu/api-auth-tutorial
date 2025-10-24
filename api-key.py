e8a41b9206b1a5e8eab1b3dd9dab0650daf7ee26
Dhruvasu <dhruvasu1@gmail.com>
2025-10-24 13:10:19 -0400

    add comment

--- api-key.py
+++ api-key.py
@@ -2,6 +2,8 @@ import requests
 
 user_id = 123
 
+# mock API. It's bad practice to expose an API key like that but this does nothing 
+# and is only a tutorial on how API keys work
 response = requests.request("GET", "https://api.mockapi.com/api/v1/user/{user_id}",
     headers={"x-api-key": "bd41b04c329chkjkj59f98454545"})
 
